6f49ec577b3b257f84f9f8ce1c19b8e9d0fdd3a7
Dominik Schloesser <dsc@dosc.net>
2018-06-16 14:45:42 +0200

    start of display-form-parser

--- samples/borgclock/src/borgclock.cpp
+++ samples/borgclock/src/borgclock.cpp
@@ -2,11 +2,6 @@
 
 #include "platform.h"
 
-#include "scheduler.h"
-#include "net.h"
-#include "mqtt.h"
-#include "ota.h"
-
 #if defined(__ESP32__)
 #define LED_BUILTIN (5)
 #endif
@@ -22,19 +17,29 @@ static const uint8_t D5 = 14;
 static const uint8_t D6 = 12;
 static const uint8_t D7 = 13;
 static const uint8_t D8 = 15;
+static const uint8_t D12 = 6;
+static const uint8_t D13 = 7;
+static const uint8_t D14 = 5;
 static const uint8_t RX = 3;
 static const uint8_t TX = 1;
 #endif
 
+#include "scheduler.h"
+#include "net.h"
+#include "mqtt.h"
+#include "ota.h"
+
 #include "clock7seg.h"
 #include "lumin.h"
-
-#include <ESP8266WiFi.h>
+#include "switch.h"
+#ifndef HUZZAH
 #include "dhtxx.h"
 #include "pressure.h"
-#include "switch.h"
 #include "airqual.h"
 #include "oled.h"
+#else
+#include "texttrans.h"
+#endif
 
 ustd::Scheduler sched;
 ustd::Net net(LED_BUILTIN);
@@ -42,21 +47,40 @@ ustd::Mqtt mqtt;
 ustd::Ota ota;
 // autbrightness=true, look for tsl2561/unitluminosity messages to adapt
 // brightness.
+#ifdef HUZZAH
+ustd::Clock7Seg clock7("clock", 0x70, D13, true, "tsl2561/unitluminosity");
+ustd::Switch sw2("sw2", D14, 20, ustd::Switch::customtopic_t::ON,
+                 "clock/alarm/off");
+#else
 ustd::Clock7Seg clock7("clock", 0x70, D5, true, "tsl2561/unitluminosity");
-ustd::Lumin lumin("tsl2561", 0x39, "1x", "fast", 28.0);
-ustd::Dht dht("dht22", 0, DHT22);  // port 0 == D3
-ustd::Pressure pressure("bmp085");
-ustd::Switch sw1("sw1", D7);
 ustd::Switch sw2("sw2", D6, 20, ustd::Switch::customtopic_t::ON,
                  "clock/alarm/off");
+#endif
+ustd::Lumin lumin("tsl2561", 0x39, "1x", "fast", 28.0);
+#ifndef HUZZAH
+ustd::Dht dht("dht22", D3, DHT22);  // port 0 == D3
+ustd::Pressure pressure("bmp085");
+ustd::Switch sw1("sw1", D7);
 ustd::AirQuality airq("ccs811");
 ustd::Oled oled("ssd1306", 0x3c, true, "tsl2561/unitluminosity");
+#endif
+ustd::TextTrans tt;
 
 void subsMsg(String topic, String msg, String originator) {
 #ifdef USE_SERIAL_DBG
     String me = topic + " " + msg + " " + originator;
     Serial.println(me.c_str());
 #endif
+}
+
+void testStuff() {
+    int n = tt.registerForm("{12345678901234567890}{2}{3}{4}{}");
+    char buf[32];
+    sprintf(buf, "%d", n);
+    sched.publish("form-count", buf);
+    for (int i = 0; i < n; i++) {
+        sched.publish("form-parms", ">" + tt.topics[i] + "<");
+    }
 }
 
 void setup() {
@@ -78,21 +102,25 @@ void setup() {
     // Workaround: relax clock stretch limit from 230 (default) to 460.
     // See: https://forums.adafruit.com/viewtopic.php?f=19&t=121816&start=15
     // twi_setClockStretchLimit(460);
-    Wire.setClockStretchLimit(460);
+    // Wire.setClockStretchLimit(460); (<- doesn't help!)
     sched.subscribe(SCHEDULER_MAIN, "borgclock/#", subsMsg);
 
     net.begin(&sched);
     mqtt.begin(&sched);
     ota.begin(&sched);
     clock7.begin(&sched);
-    clock7.maxAlarmDuration = 10;
+    clock7.maxAlarmDuration = 10;  // switch off alarm after 10sec.
     lumin.begin(&sched);
+#ifndef HUZZAH
     dht.begin(&sched);
     pressure.begin(&sched);
     sw1.begin(&sched);
     sw2.begin(&sched);
     airq.begin(&sched);
     oled.begin(&sched);
+#else
+    sched.add(testStuff, "tester", 4000000);
+#endif
 }
 
 void loop() {
--- tinkers/texttrans.h
+++ tinkers/texttrans.h
@@ -1,5 +1,7 @@
 #include "scheduler.h"
+#include "array.h"
 
+namespace ustd {
 // Form: This is a sample Form\nCpu: %5s Temp: %5s\n
 // Vars: ["val/cpu", "bla/temperature"];
 // [{0,0,1,"@time"},{1,0,1,"Cpu:"}]
@@ -7,13 +9,44 @@
 // "T1 {topic/temperature} T2 {topic2/temperature}"
 class TextTrans {
   public:
-    ustd::map<String, String> topic2value;
+    // ustd::map<String, String> topic2value;
+    ustd::array<String> topics;
 
     TextTrans() {
     }
     ~TextTrans() {
     }
 
-    void registerForm(String form) {
+    // get all substrings delimited by c1,c2 from source and put them
+    // into array pDest. Return number of tokens found.
+    // e.g. source="asd {a} {abc} {def} sdf" with c1,c2 = '{', '}' should
+    // give array ["a","abc","def"]
+    int getDelimitedTokens(String source, char c1, char c2,
+                           ustd::array<String> *pDest) {
+        bool finished = false;
+        int i1, i2;
+        String src = source;
+        int nr = 0;
+        while (!finished) {
+            i1 = src.indexOf(c1);
+            i2 = src.substring(i1 + 1).indexOf(c2);
+            if ((i1 == -1) || (i2 == -1)) {
+                finished = true;
+            } else {
+                String sub = src.substring(i1 + 1, i2 + i1 + 1);
+                src = src.substring(i1 + 1 + i2 + 1);
+                pDest->add(sub + "|" + src);
+                ++nr;
+            }
+        }
+        return nr;
+    }
+
+    int registerForm(String form) {
+        while (!topics.isEmpty()) {
+            topics.erase(0);
+        }
+        return getDelimitedTokens(form, '{', '}', &topics);
     }
 };
+}  // namespace ustd
